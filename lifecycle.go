c0582f7740986ace05014c89dd0d7244682b81db
Bob Uhl <buhl@zvelo.com>
2018-07-20 12:33:10 -0400

    Listen for desired signals
    Make a copy of DefaultSignals; don't do a zero-length copy (which
    leads to listening for _all_ signals!).

--- lifecycle.go
+++ lifecycle.go
@@ -82,7 +82,7 @@ func New(ctx context.Context, opts ...Option) (context.Context, Manager) {
 	h := &manager{
 		deferred: []func() error{},
 	}
-	copy(h.sigs, DefaultSignals)
+	h.sigs = DefaultSignals[:]
 	h.ctx, h.cancel = context.WithCancel(ctx)
 	h.Group, h.gctx = errgroup.WithContext(context.Background())
 	for _, o := range opts {
